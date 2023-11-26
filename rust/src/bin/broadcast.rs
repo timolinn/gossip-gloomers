026d8511a5fbb880141f5d1b3500f26d5d618a0e
Tim <11758061+timolinn@users.noreply.github.com>
2023-11-26 09:52:22 +0100

    refactoring

--- rust/src/bin/broadcast.rs
+++ rust/src/bin/broadcast.rs
@@ -44,7 +44,7 @@ impl Node<(), Payload> for BroadcastNode {
     {
         let node_id = init.node_id.clone();
         thread::spawn(move || loop {
-            thread::sleep(Duration::from_millis(1000));
+            thread::sleep(Duration::from_millis(6000));
             let msg = Message::new(
                 node_id.as_str().to_string(),
                 node_id.as_str().to_string(),
@@ -76,6 +76,9 @@ impl Node<(), Payload> for BroadcastNode {
         let mut reply = input.into_reply(Some(&mut self.id));
         match reply.body.payload {
             Payload::Broadcast { message } => {
+                reply.body.payload = Payload::BroadcastOk;
+                reply.send(output).context("failed to send message")?;
+
                 if !self.messages.contains(&message) {
                     self.messages.push(message);
                     for neighbor in &self.neighbors {
@@ -100,9 +103,6 @@ impl Node<(), Payload> for BroadcastNode {
                         self.waiting_for_ack
                             .insert(broad_msg.body.id.unwrap(), broad_msg);
                     }
-
-                    reply.body.payload = Payload::BroadcastOk;
-                    reply.send(output).context("failed to send message")?;
                 }
             }
             Payload::BroadcastOk => {
@@ -121,24 +121,24 @@ impl Node<(), Payload> for BroadcastNode {
                 };
                 reply.send(output).context("failed to send message")?;
             }
-            Payload::ReadOk { .. } => {}
-            Payload::Topology { topology } => {
-                self.neighbors = topology[&self.node].clone();
+            Payload::Topology { mut topology } => {
+                self.neighbors = topology.remove(&self.node).unwrap_or_else(|| {
+                    panic!("could not retrieve topology for node: {}", &self.node)
+                });
                 reply.body.payload = Payload::TopologyOk;
                 reply.send(output).context("failed to send message")?;
             }
-            Payload::TopologyOk => {}
             Payload::BroadcastUnAcked => {
+                // let l = self
+                //     .waiting_for_ack
+                //     .iter()
+                //     .map(|x| x.1)
+                //     .collect::<Vec<&Message<Payload>>>();
                 for m in self.waiting_for_ack.values() {
-                    let msg = m.clone();
-                    msg.send(output).context("failed to send message")?;
+                    m.send(output).context("failed to send message")?;
                 }
-            } // _ => {
-              //     // if reply.body.payload == Payload::Init {
-              //     //     return Ok(());
-              //     // }
-              //     eprintln!("not supported: {:?}", reply.body.payload)
-              // }
+            }
+            Payload::TopologyOk | Payload::ReadOk { .. } => {}
         }
         Ok(())
     }
